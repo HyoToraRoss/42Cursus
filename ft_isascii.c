c00fb5552fd20fd99483fd67066b832ff480e246
HyoToraRoss <111071537+HyoToraRoss@users.noreply.github.com>
2022-10-12 12:20:02 +0100

    ft_isascii

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,31 +6,13 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 21:50:43 by martavar          #+#    #+#             */
-/*   Updated: 2022/10/07 22:30:59 by martavar         ###   ########.fr       */
+/*   Updated: 2022/10/12 12:18:24 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isascii(int c)
 {
-	if (0 <= c && c <= 0177)
+	if (0 <= c && c <= 127)
 		return (1);
 	return (0);
 }
-
-#include <stdio.h>
-#include <ctype.h>
-
-char chars[] = {'A', 0x80, 'Z'};
-
-#define SIZE sizeof( chars ) / sizeof( char )
-
-void main()
-  {
-    int   i;
-
-    for( i = 0; i < SIZE; i++ ) {
-      printf( "Char %c is %san ASCII character\n",
-        chars[i],
-        ( isascii( chars[i] ) ) ? "" : "not " );
-    }
-  }

5ab8b794ad9a80fa250509a20de6abc786f2f2ea
HyoToraRoss <111071537+HyoToraRoss@users.noreply.github.com>
2022-10-20 17:58:16 +0100

    ft_strrchr

---
+++ Libft/ft_strrchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 13:52:43 by martavar          #+#    #+#             */
+/*   Updated: 2022/10/20 17:56:02 by martavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = ft_strlen(s);
+	if (c == '\0')
+		return ((char *)&s[i]);
+	i--;
+	while (i >= 0)
+	{
+		if (s[i] == c)
+			return ((char *)&s[i]);
+		i--;
+	}
+	return (NULL);
+}

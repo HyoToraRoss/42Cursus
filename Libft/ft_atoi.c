8c280494a3fe791045d82c29526d4a5168c2e6a5
HyoToraRoss <neko.ross@gmail.com>
2022-10-27 13:48:05 +0100

    atoi

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 10:28:33 by martavar          #+#    #+#             */
-/*   Updated: 2022/10/27 11:50:34 by martavar         ###   ########.fr       */
+/*   Updated: 2022/10/27 13:47:50 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,21 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	result = 1;
 	end = 0;
-	while (nptr[i])
+	while (nptr[i] == ' ' || nptr[i] == '\n' || nptr[i] == '\t' 
+		|| nptr[i] == '\v' || nptr[i] == '\f' || nptr[i] == '\r')
+		i++;
+	while (nptr[i] == '+' || nptr[i] == '-')
 	{
-		while (nptr[i] == ' ' && nptr[i] == '\v' && nptr[i] == '\r')
-			i++;
 		if (nptr[i] == '-')
 			result *= -1;
 		if (!('0' <= nptr[i + 1] && nptr[i + 1] <= '9'))
 			return (0);
 		i++;
-		while ('0' <= nptr[i] && nptr[i] <= '9')
-		{
+	}
+	while ('0' <= nptr[i] && nptr[i] <= '9')
+	{
 		end = end * 10 + (nptr[i] - '0');
 		i++;
-		}
 	}
 	return (end * result);
 }
@@ -43,7 +44,7 @@ int	ft_atoi(const char *nptr)
 
 int main (void)
 {
-	char	nptr[] = "    -7846528bjhfej";
+	char	nptr[] = "    784awef6528bjhfej";
 
 	printf("%i\n", ft_atoi(nptr));
 	printf("%i\n", atoi(nptr));

53f4a9a78f26fc5be7805ae95b82cfedb931a167
HyoToraRoss <neko.ross@gmail.com>
2022-11-04 18:14:10 +0000

    Part2

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -1,38 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/04 17:15:00 by martavar          #+#    #+#             */
-/*   Updated: 2022/11/04 18:02:35 by martavar         ###   ########.fr       */
+/*   Created: 2022/11/04 18:03:21 by martavar          #+#    #+#             */
+/*   Updated: 2022/11/04 18:11:24 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
+	size_t	len_s1;
+	size_t	len_s2;
 	size_t	i;
+	size_t	j;
+	char	*str;
 
-	if (!s)
-		return (NULL);
-	if (len < start)
-		return (NULL);
-	str = malloc(sizeof(char) * (len + 1));
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	str = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 	if (!str)
 		return (NULL);
 	i = 0;
-	if (s)
+	while (s1)
 	{
-		while (i < len)
-		{
-			str[i] = s[start + i];
-			i++;
-		}
+		str[i] = s1[i];
+		i++;
 	}
-	str[i] = '\0';
+	j = 0;
+	while (s2)
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
 	return (str);
 }

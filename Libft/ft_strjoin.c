bd4aa8d99038327db925e64aa97e6ff94cb585a7
HyoToraRoss <neko.ross@gmail.com>
2022-11-04 18:21:28 +0000

    Part2

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 18:03:21 by martavar          #+#    #+#             */
-/*   Updated: 2022/11/04 18:11:24 by martavar         ###   ########.fr       */
+/*   Updated: 2022/11/04 18:19:54 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	len_s1;
-	size_t	len_s2;
+	size_t	len;
 	size_t	i;
 	size_t	j;
 	char	*str;
 
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	str = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (s1)
+	while (s1[i])
 	{
 		str[i] = s1[i];
 		i++;
 	}
 	j = 0;
-	while (s2)
+	while (s2[j])
 	{
 		str[i + j] = s2[j];
 		j++;

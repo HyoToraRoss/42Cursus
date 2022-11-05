23a6b93e362b6f47d30f952c4d031132f067c5ec
Maria Catarina Tavares  Rodrigues De Sousa <martavar@c1r3s3.42lisboa.com>
2022-11-05 19:32:08 +0000

    Part2

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 17:15:00 by martavar          #+#    #+#             */
-/*   Updated: 2022/11/04 22:40:37 by martavar         ###   ########.fr       */
+/*   Updated: 2022/11/05 19:11:34 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,21 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	size_t	i;
-	size_t	len_s;
 
 	if (!s)
 		return (NULL);
-	if (len < start)
-		return (NULL);
+	i = 0;
+	if (ft_strlen(s) < start)
+	{
+		str = malloc(sizeof(char));
+		*str = '\0';
+		return (str);
+	}
+	if (len >= ft_strlen(s))
+		len = (ft_strlen(s) - start);
 	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
-	i = 0;
-	len_s = ft_strlen(s);
-	if (len_s <= start)
-	{
-		while (i < len)
-		{
-			str[i] = '\0';
-			i++;
-		}
-	}
 	while (i < len)
 	{
 		str[i] = s[start + i];

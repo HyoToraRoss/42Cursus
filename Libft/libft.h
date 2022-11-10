4e6d6f2350cfd4dcc350df6a6e77bec25bf2eb9d
HyoToraRoss <neko.ross@gmail.com>
2022-11-10 18:33:46 +0000

    Bonus

--- Libft/libft.h
+++ Libft/libft.h
@@ -57,5 +57,6 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif

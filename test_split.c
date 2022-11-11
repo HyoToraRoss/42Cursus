38d7a33a6c4ae5db51476259f9a30ebb7913055c
HyoToraRoss <neko.ross@gmail.com>
2022-11-11 17:33:27 +0000

    test

---
+++ test_split.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_split.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 17:30:17 by martavar          #+#    #+#             */
+/*   Updated: 2022/11/11 17:32:39 by martavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	**ft_split(char const *str, char c)
+{
+	char	*split;
+	int		start;
+	int		end;
+	int		i;
+
+	split = malloc(sizeof(char *) * size(str,))
+	end = 0;
+	while (str[end])
+	{
+
+	}
+}

bd2916bb734d1846486dd8790be5ea8d2f0adc4f
HyoToraRoss <neko.ross@gmail.com>
2022-11-17 15:19:32 +0000

    Printf

---
+++ Printf/ft_printf.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 14:43:42 by martavar          #+#    #+#             */
+/*   Updated: 2022/11/17 15:16:03 by martavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_printf(const char *, ...)
+{
+
+}
+
+/*
+%c Prints a single character.
+%s Prints a string (as defined by the common C convention).
+%p The void * pointer argument has to be printed in hexadecimal format.
+%d Prints a decimal (base 10) number.
+%i Prints an integer in base 10.
+%u Prints an unsigned decimal (base 10) number.
+%x Prints a number in hexadecimal (base 16) lowercase format.
+%X Prints a number in hexadecimal (base 16) uppercase format.
+%% Prints a percent sign.
+*/
---
+++ Printf/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 15:12:33 by martavar          #+#    #+#             */
+/*   Updated: 2022/11/17 15:15:56 by martavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stddef.h>
+
+int	ft_printf(const char *, ...);
+
+#endif

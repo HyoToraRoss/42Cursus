9f31e8f4bc4c4dacb3c9360bab5f6642493119d8
HyoToraRoss <neko.ross@gmail.com>
2022-11-26 11:48:56 +0000

    get

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,57 +6,11 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 17:51:59 by martavar          #+#    #+#             */
-/*   Updated: 2022/11/21 15:00:13 by martavar         ###   ########.fr       */
+/*   Updated: 2022/11/26 10:32:01 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-static char	*function_name(int fd, char *buf, char *backup)
-{
-	int		read_line;
-	char	*char_temp;
-
-	read_line = 1;
-	while (read_line != '\0')
-	{
-		read_line = read(fd, buf, BUFFER_SIZE);
-		if (read_line == -1)
-			return (0);
-		else if (read_line == 0)
-			break ;
-		buf[read_line] = '\0';
-		if (!backup)
-			backup = ft_strdup("");
-		char_temp = backup;
-		backup = ft_strjoin(char_temp, buf);
-		free(char_temp);
-		char_temp = NULL;
-		if (ft_strchr (buf, '\n'))
-			break ;
-	}
-	return (backup);
-}
-
-static char	*extract(char *line)
-{
-	size_t	count;
-	char	*backup;
-
-	count = 0;
-	while (line[count] != '\n' && line[count] != '\0')
-		count++;
-	if (line[count] == '\0' || line[1] == '\0')
-		return (0);
-	backup = ft_substr(line, count + 1, ft_strlen(line) - count);
-	if (*backup == '\0')
-	{
-		free(backup);
-		backup = NULL;
-	}
-	line[count + 1] = '\0';
-	return (backup);
-}
 
 char	*get_next_line(int fd)
 {
@@ -76,4 +30,19 @@ char	*get_next_line(int fd)
 		return (NULL);
 	backup = extract(line);
 	return (line);
+}
+
+#include <stdio.h>
+
+int main()
+{
+	FILE *the_file = fopen("the_text_file.txt", "r");
+	if (the_file == NULL)
+	{
+		perror("Unable to open the file");
+		exit (1);
+	}
+	char	line[100];
+	while(fgets(line, ))
+	return (0);
 }
--- GetNextLine/get_next_line.h
+++ GetNextLine/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 17:53:45 by martavar          #+#    #+#             */
-/*   Updated: 2022/11/21 14:49:24 by martavar         ###   ########.fr       */
+/*   Updated: 2022/11/26 10:21:48 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,5 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s1);
-size_t	ft_strlen(const char *c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif

e9e172ad5d00129999c9abdb81c171dabd9b0ffd
HyoToraRoss <neko.ross@gmail.com>
2022-11-27 17:30:38 +0000

    gnl

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,50 +6,34 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 17:51:59 by martavar          #+#    #+#             */
-/*   Updated: 2022/11/26 17:22:21 by martavar         ###   ########.fr       */
+/*   Updated: 2022/11/27 17:29:27 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/*
 char	*get_next_line(int fd)
 {
 	char		*line;
-	char		*buf;
-	static char	*backup;
+	static char	*buff;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (0);
-	line = function_name(fd, buf, backup);
-	free(buf);
-	buf = NULL;
-	if (!line)
-		return (NULL);
-	backup = extract(line);
+	
 	return (line);
 }
-*/
 
-#include <stdio.h>
-
-int main()
+int	main(void)
 {
-	FILE *the_file = fopen("the_text_file.txt", "r");
-	if (the_file == NULL)
-	{
-		perror("Unable to open the file");
-		exit (1);
-	}
-	char	line[100];
-	while(fgets(line, sizeof(line), the_file))
-	{
-		//print the line
-		printf("%s", line);
-	}
-	fclose(the_file);                                                                                                                                        
+	int		fd;
+	char	*line;
+
+	fd = open("the_text_file.txt", O_RDWR);
+	line = get_next_line(fd);
+	printf("%s", line);
+	close(fd);
 	return (0);
 }
--- GetNextLine/get_next_line.h
+++ GetNextLine/get_next_line.h
@@ -6,15 +6,22 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 17:53:45 by martavar          #+#    #+#             */
-/*   Updated: 2022/11/26 10:21:48 by martavar         ###   ########.fr       */
+/*   Updated: 2022/11/27 17:28:29 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
 # include <stdlib.h>
+# include <stdio.h>
 # include <unistd.h>
+# include <limits.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
 

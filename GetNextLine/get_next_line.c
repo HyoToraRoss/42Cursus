a45114c5ae6b74fe289610997d3b49bd06d8aced
HyoToraRoss <neko.ross@gmail.com>
2022-11-28 17:54:33 +0000

    gnl

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,91 +6,50 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 17:51:59 by martavar          #+#    #+#             */
-/*   Updated: 2022/11/27 18:35:13 by martavar         ###   ########.fr       */
+/*   Updated: 2022/11/28 17:54:02 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_limpamemo(char *buff)
+char	*ft_strdup(const char *s1)
 {
-	int	i;
-	int	is_nline;
-	int	j;
-
-	i = 0;
-	j = 0;
-	is_nline = 0;
-	while (buff[i])
-	{
-		if (is_nline)
-			buff[j++] = buff[i];
-		if (buff[i] == '\n')
-			is_nline = 1;
-		buff[i++] = '\0';
-	}
-	return (is_nline);
-}
-
-size_t	ft_strlen(const char *c)
-{
-	int	i;
-
-	i = 0;
-	while (c[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	size_t	len;
-	size_t	i;
-	size_t	j;
 	char	*str;
+	int		i;
+	int		len;
 
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2);
+	len = 0;
+	while (s1[len] != '\0')
+		len++;
 	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (s1[i])
+	while (s1[i] != '\0')
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	j = 0;
-	while (s2[j])
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
-	str[i + j] = '\0';
+	str[i] = '\0';
 	return (str);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*buff;
+	static char	*buf;
 	char		*line;
-	int			i;
 
-	i = 0;
-	if (BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
-	{
-		while (buff[i])
-			buff[i++] = 0;
+	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
 		return (NULL);
-	}
 	line = NULL;
-	while (buff || read(fd, buff, BUFFER_SIZE) > 0)
-	{
-		line = ft_strjoin(line, buff);
-		if (ft_limpamemo(buff))
-			break ;
-	}
+	// read from fd and add to the stash
+	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (buf == NULL)
+		return (NULL);
+	read(fd, buf, BUFFER_SIZE);
+	// extract to stash to line
+	// clean up stash
+	line = ft_strdup(buf);
 	return (line);
 }
 
@@ -100,8 +59,14 @@ int	main(void)
 	char	*line;
 
 	fd = open("the_text_file.txt", O_RDONLY);
-	line = get_next_line(fd);
+	//while (1)
+	//{
+		line = get_next_line(fd);
+		//if (line == NULL)
+		//	break;
 	printf("%s", line);
+	free(line);
+	//}
 	close(fd);
 	return (0);
 }

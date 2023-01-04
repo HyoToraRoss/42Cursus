541e89c0f735ccb204549028fde7b21a78ae6089
HyoToraRoss <neko.ross@gmail.com>
2023-01-04 10:34:57 +0000

    ...

--- ExamRank2/Level0/first_word/first_word.c
+++ ExamRank2/Level0/first_word/first_word.c
@@ -1,28 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   first_word.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/20 12:29:45 by martavar          #+#    #+#             */
-/*   Updated: 2023/01/03 17:00:41 by martavar         ###   ########.fr       */
+/*   Created: 2023/01/03 17:14:22 by martavar          #+#    #+#             */
+/*   Updated: 2023/01/04 10:34:36 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h" 
+#include <unistd.h>
 
-void sighandler(int signum) {
-   printf("Caught signal %d, coming out...\n", signum);
-   exit(1);
-}
+int	main(int argc, char **argv)
+{
+	int	i;
 
-int main () {
-   signal(SIGINT, sighandler);
-
-   while(1) {
-      printf("Going to sleep for a second...\n");
-      sleep(1); 
-   }
-   return(0);
+	i = 0;
+	if (argc == 2)
+	{
+		i = 0;
+		while (argv[1][i] == ' ' || argv[1][i] == '\f' || argv[1][i] == '\n' 
+			|| argv[1][i] == '\r' || argv[1][i] != '\t' || argv[1][i] != '\v')
+		{
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+		write(1, "\n", 1);
+	}
+	return (0);
 }

eebc8c9aa1ab4acc7e7b5e8076acfe26cf0532a7
HyoToraRoss <neko.ross@gmail.com>
2023-01-13 13:19:49 +0000

    sort_int_tab

---
+++ ExamRank2/Level4/sort_int_tab/sort_int_tab.c
@@ -0,0 +1,18 @@
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int i;
+	int	temp;
+
+	i = 0;
+	while(i < size - 1)
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			temp = tab[i];
+			tab[i] = tab[i + 1];
+			i = 0;
+		}
+		else 
+			i++;
+	}
+}

3d52b93d1362024aedfe03b8bae0f39acb2212ff
HyoToraRoss <neko.ross@gmail.com>
2023-01-15 20:17:40 +0000

    minitalk

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -6,7 +6,31 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 12:29:16 by martavar          #+#    #+#             */
-/*   Updated: 2022/12/20 12:29:24 by martavar         ###   ########.fr       */
+/*   Updated: 2023/01/15 20:03:30 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+int	main(int argc, char **argv)
+{
+	struct sigaction sa;
+	sa.sa_handler = &handle_sigtstp;
+	{
+		/* data */
+	};
+	
+	if (argc == 3)
+	{
+		kill(argv[1], )
+	}
+		return (-1);
+	
+}
+/*struct sigaction {
+               void     (*sa_handler)(int);
+               void     (*sa_sigaction)(int, siginfo_t *, void *);
+               sigset_t   sa_mask;
+               int        sa_flags;
+               void     (*sa_restorer)(void);
+           };*/
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -6,23 +6,47 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 12:29:45 by martavar          #+#    #+#             */
-/*   Updated: 2023/01/03 17:00:41 by martavar         ###   ########.fr       */
+/*   Updated: 2023/01/15 19:44:23 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h" 
 
-void sighandler(int signum) {
-   printf("Caught signal %d, coming out...\n", signum);
-   exit(1);
+/*
+void	signal_handler(int signum)
+{
+	printf("Received SIGINT!\n", signum);
+	exit (0);
 }
+*/
 
-int main () {
-   signal(SIGINT, sighandler);
+int	main(void)
+{
+	pid_t		pid;
+	//sigset_t signal_set;
+	pid = getpid();
+	printf("The process ID is %d\n", pid);
+	/*
+	struct sigaction {
+               void     (*sa_handler)(int);
+               void     (*sa_sigaction)(int, siginfo_t *, void *);
+               sigset_t   sa_mask;
+               int        sa_flags;
+               void     (*sa_restorer)(void);
+           };
+		//Set the signal handler for the SIGINT and SIGTERM signals
+	//to the signal_handler function
+	signal(SIGINT, signal_handler);
+	signal(SIGTERM, signal_handler);
+	//Initialize an empty signal set
+	sigemptyset(&signal_set);
+	//Add SIGINT to the signal_set
+	sigaddset(&signal_set, SIGINT);
+	while (1)
+	{
+		//Some work to do
+	}
+	*/
 
-   while(1) {
-      printf("Going to sleep for a second...\n");
-      sleep(1); 
-   }
-   return(0);
+	return (0);
 }

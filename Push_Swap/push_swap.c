0c547a0b318b215c8c111f6700f7a10cb8216cdb
HyoToraRoss <neko.ross@gmail.com>
2023-02-11 19:07:28 +0000

    push_swap

---
+++ Push_Swap/push_swap.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/11 19:05:32 by martavar          #+#    #+#             */
+/*   Updated: 2023/02/11 19:07:14 by martavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+Create two lists
+
+Verify the input, if it has any invalid character
+
+Do a normal swap and see if all the movements work
+
+After all that works well, I can think about the logic

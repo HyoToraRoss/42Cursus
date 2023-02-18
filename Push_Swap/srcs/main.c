502751ec1f0d0ccb72b2ccec14cea4a067747753
HyoToraRoss <neko.ross@gmail.com>
2023-02-18 19:54:57 +0000

    push_swap

--- Push_Swap/srcs/main.c
+++ Push_Swap/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 19:05:32 by martavar          #+#    #+#             */
-/*   Updated: 2023/02/18 17:19:46 by martavar         ###   ########.fr       */
+/*   Updated: 2023/02/18 19:54:23 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,25 @@
 
 #include <push_swap.h>
 
+
+void	clean_stack(t_stk	**s)
+{
+	t_stk	*tmp;
+
+	if (*s)
+	{
+		while (*s)
+		{
+			tmp = (*s)->next;
+			free(*s);
+			*s = NULL;
+			(*s) = tmp;
+		}
+		free(*s);
+		*s = NULL;
+	}
+}
+
 t_stk	*create_new_node(int nb)
 {
 	t_stk *s;
@@ -32,26 +51,29 @@ t_stk	*create_new_node(int nb)
 	return (s);
 }
 
-int	main(int ac, char **av)
+int	main(int argc, char **argv)
 {
 	t_stk	*stk_a;
 	t_stk	*begin;
 	//t_stk *stk_b;
 
-	(void)ac;
-	(void)av;
 	stk_a = NULL;
 	begin = NULL;
-	int	i = 0;
-	while (i++ < 10)
+	int	i = 1;
+	if (argc > 1)
 	{
-		stk_a = create_new_node(i);
-		stk_a->next = begin;
-		begin = stk_a;
+		while (argv[i])
+		{
+			stk_a = create_new_node(ft_atoi(argv[i]));
+			stk_a->next = begin;
+			begin = stk_a;
+			i++;
+		}
+		while (begin)
+		{
+			printf("%d\n", begin->nbr);
+			begin = begin->next;
+		}
 	}
-	while (begin)
-	{
-		printf("%d\n", begin->nbr);
-		begin = begin->next;
-	}
+	clean_stack(&stk_a);
 }

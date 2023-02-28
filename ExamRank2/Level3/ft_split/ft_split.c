de21053f55532296b95df4512569626e84514649
HyoToraRoss <neko.ross@gmail.com>
2023-02-28 22:58:04 +0000

    ft_split

--- ExamRank2/Level3/ft_split/ft_split.c
+++ ExamRank2/Level3/ft_split/ft_split.c
@@ -1,36 +1,103 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 
-int count_words(char *str)
+int		word_counter(char *str)
 {
-	int num_words = 0;
-	while(*str == ' ' || )
+	int i;
+	int checker;
+	int count;
+
+	i = 0;
+	checker = 0;
+	count = 0;
+	while (str[i])
+	{
+		while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		{
+			i++;
+			checker = 0;
+		}
+		while (str[i] > 32 && str[i] < 127)
+		{
+			if (checker == 0)
+			{
+				count++;
+				checker = 1;
+			}
+			i++;
+		}
+	}
+	return (count);
+}
+
+char	*sub_str_extracter(char *str)
+{
+	char	*sub_str;
+	int		i;
+	int		len;
+
+	i = 0;
+	len = 0;
+	while (str[len] && (str[len] > 32 && str[len] < 127))
+		len++;
+	sub_str = malloc(sizeof(char) * (len + 1));
+	if (!sub_str)
+		return NULL;
+	while (str[i] && (str[i] > 32 && str[i] < 127))
+	{
+		sub_str[i] = str[i];
+		i++;
+	}
+	sub_str[i] = '\0';
+	return (sub_str);
 }
 
 char	**ft_split(char *str)
 {
-	int	num_words;
-	char **split;
+	char	**array;
+	int		size_arr;
+	int		j;
+	int		checker;
 
-	num_words = count_words(str);
-	split = malloc(sizeof(char *) * num_words + 1);
-
-	split[num_words] = 0;
-	fill_str(split, str);
-	return (split);
+	j = 0;
+	checker = 0;
+	size_arr = word_counter(str);
+	array = malloc(sizeof(char*) * (size_arr + 1));
+	if (!array)
+		return NULL;
+	while (*str)
+	{
+		while (*str == 32 || (*str >= 9 && *str <= 13))
+		{
+			str++;
+			checker = 0;
+		}
+		while (*str > 32 && *str < 127)
+		{
+			if (checker == 0)
+			{
+				array[j] = sub_str_extracter(str);
+				j++;
+				checker = 1;
+			}
+			str++;
+		}
+	}
+	array[j] = 0;
+	return (array);
 }
 
-int	main(void)
+
+int		main()
 {
-	char	**tab;
-	unsigned int	i;
+	char	**array;
+	int		i;
 
 	i = 0;
-	tab = ft_split(" hello, split   this,plz,  ");
-	while (tab[i] != NULL)
+	array = ft_split("a   b   c    d");
+	while (array[i])
 	{
-		write(1, &tab[i], 1);
+		printf("str:%s\n", array[i]);
 		i++;
 	}
 }

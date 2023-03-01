ed3542537ed82266ea185b97d91aecfcb6907921
HyoToraRoss <111071537+HyoToraRoss@users.noreply.github.com>
2023-03-01 17:13:28 +0000

    ft_strcspn

---
+++ ExamRank2/Level1/ft_strcspn/ft_strcspn.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+size_t  ft_strcspn(const char *s, const char *reject)
+{
+	int	i = 0;
+	int	j = 0;
+
+	while (s[i])
+	{
+		j = 0;
+		while (reject[j])
+		{
+			if (s[i] == reject[j])
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
+
+int main()
+{
+    printf("%ld\n", ft_strcspn("ola", "e"));
+    printf("%ld\n", strcspn("ola", "e"));
+}

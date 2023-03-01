8fe90ca5c07be4041bd74eb5f6e84a9d8b061dc7
HyoToraRoss <111071537+HyoToraRoss@users.noreply.github.com>
2023-03-01 18:34:05 +0000

    reverse_bits

--- ExamRank2/Level2/reverse_bits/reverse_bits.c
+++ ExamRank2/Level2/reverse_bits/reverse_bits.c
@@ -1,4 +1,51 @@
+#include <stdio.h>
+
 unsigned char	reverse_bits(unsigned char octet)
 {
-	
+	unsigned char byte;
+	int i;
+	int n;
+	int power;
+
+	byte = 0;
+	i = 7;
+	while (i >= 0)
+	{
+		if (octet << i & 128)
+		{
+			n = 0;
+			power = 1;
+			if (i == 0)
+				power = 1;
+			while (n < i)
+			{
+				power *= 2;
+				n++;
+			}
+			byte += power;
+		}
+		i--;
+	}
+	return (byte);
+}
+
+/* another form to do */
+unsigned char	reverse_bits(unsigned char octet)
+{
+	int		i = 8;
+	unsigned char	res = 0;
+
+	while (i > 0)
+	{
+		res = res * 2 + (octet % 2);
+		octet = octet / 2;
+		i--;
+	}
+	return (res);
+}
+
+int main()
+{
+	printf("%i\n", reverse_bits(2));
+	printf("%i\n", reverse_bits2(2));
 }

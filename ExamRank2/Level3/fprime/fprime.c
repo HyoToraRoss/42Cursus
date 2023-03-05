5d7cd3d8104cf2927b82cfd6d86a40f56c84b1ab
HyoToraRoss <neko.ross@gmail.com>
2023-03-05 00:29:29 +0000

    fprime

---
+++ ExamRank2/Level3/fprime/fprime.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		int	i = 1;
+		int nbr = atoi(argv[1]);
+
+		if (nbr == 1)
+			printf("1");
+		while (nbr >= ++i)
+		{
+			if (nbr % i == 0)
+			{
+				printf("%d", i);
+				if (nbr == i)
+					break ;
+				printf("*");
+				nbr /= i;
+				i = 1;
+			}
+		}
+	}
+	printf("\n");
+}

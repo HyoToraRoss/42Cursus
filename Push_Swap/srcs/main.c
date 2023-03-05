fe98205374541890ce647d76749764f3e6f4eeac
HyoToraRoss <neko.ross@gmail.com>
2023-03-05 19:24:51 +0000

    push_swap

--- Push_Swap/srcs/main.c
+++ Push_Swap/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: martavar <martavar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 19:05:32 by martavar          #+#    #+#             */
-/*   Updated: 2023/03/05 17:23:22 by martavar         ###   ########.fr       */
+/*   Updated: 2023/03/05 19:24:40 by martavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,69 +20,3 @@
 
 #include "../includes/push_swap.h"
 
-
-void	clean_stack(t_stk	**s)
-{
-	t_stk	*tmp;
-
-	if (*s)
-	{
-		while (*s)
-		{
-			tmp = (*s)->next;
-			free(*s);
-			*s = NULL;
-			(*s) = tmp;
-		}
-		free(*s);
-		*s = NULL;
-	}
-}
-
-t_stk	*create_new_node(int nb)
-{
-	t_stk	*s;
-
-	s = malloc(sizeof(t_stk));
-	if (!s)
-		return (0);
-	s->nbr = nb;
-	s->next = NULL;
-	return (s);
-}
-
-int	main(int argc, char **argv)
-{
-	t_stk	*stk_a;
-	t_stk	*begin;
-	//t_stk *stk_b;
-
-	stk_a = NULL;
-	int	i = 1;
-	if (argc > 1)
-	{
-
-		if (!stk_a)
-		{
-			stk_a = create_new_node(ft_atoi(argv[i++]));
-			begin = stk_a;
-		}
-		while (argv[i])
-		{		
-			if (!(ft_atoi(argv[i]) > -2147483648 && ft_atoi(argv[i]) <= 2147483647))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			stk_a->next = create_new_node(ft_atoi(argv[i]));
-			stk_a = stk_a->next;
-			i++;
-		}
-		while (begin)
-		{
-			printf("%d\n", begin->nbr);
-			begin = begin->next;
-		}
-	}
-	clean_stack(&stk_a);
-}

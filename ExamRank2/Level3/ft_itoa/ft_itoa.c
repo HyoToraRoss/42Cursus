405327a37cb66500da1df0059fb65443ae4216cb
HyoToraRoss <neko.ross@gmail.com>
2023-03-12 21:17:38 +0000

    ft_itoa

---
+++ ExamRank2/Level3/ft_itoa/ft_itoa.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+char	*ft_itoa(int nbr)
+{
+	int	n = nbr;
+	int	len = 0;
+
+	if (nbr <= 0)
+		len++;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	char *str = malloc(sizeof(char) * len + 1);
+	if (!str)
+		return 0;
+	str[len] = '\0';
+	if (nbr == 0)
+		str[0] = '0';
+	if (nbr < 0)
+	{
+		str[0] = '-';
+		nbr *= -1;
+	}
+	while (nbr >= 0)
+	{
+		str[--len] = nbr % 10 + '0';
+		nbr /= 10;
+	}
+	return (str);
+}
+
+int	main(void)
+{
+	printf("str:%s\n", ft_itoa(-2147483648));
+}
